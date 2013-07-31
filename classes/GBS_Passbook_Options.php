3c36a3dde4b1b9adb0c9e8dd053c6d9defbd0159
Dan Cameron <dan@sproutventure.com>
2013-07-30 17:07:39 -0700

    Options
    Updates #3539

--- classes/GBS_Passbook_Options.php
+++ classes/GBS_Passbook_Options.php
@@ -4,33 +4,37 @@
 * Creates the Passbook Options
 */
 class GBS_Passbook_Options extends GBS_Passbook_Addon {
-	const PASSWORD = 'gb_passbook_password';
-	const TYPE = 'gb_passbook_typeid';
-	const TEAMID = 'gb_passbook_teamid';
-	const LOGO = 'gb_passbook_logo';
-	const ICON = 'gb_passbook_icon';
-	const ICON2 = 'gb_passbook_icon2';
-	private static $password;
-	private static $passtype;
-	private static $teamid;
-	private static $logo;
-	private static $icon;
-	private static $icon2;
+	const PASSWORD = 'gb_passbook_vouchers_password';
+	const TYPE = 'gb_passbook_vouchers_typeid';
+	const TEAMID = 'gb_passbook_vouchers_teamid';
+	const LOGO = 'gb_passbook_vouchers_logo';
+	const ICON = 'gb_passbook_vouchers_icon';
+	const ICON2 = 'gb_passbook_vouchers_icon2';
+	const BG = 'gb_passbook_vouchers_background';
+	public static $password;
+	public static $passtype;
+	public static $teamid;
+	public static $logo;
+	public static $icon;
+	public static $icon2;
+	public static $bg;
 
 	public static function init() {
-		self::$password = get_option( self::PASSWORD );
-		self::$passtype = get_option( self::TYPE );
-		self::$teamid = get_option( self::TEAMID );
-		self::$logo = get_option( self::LOGO );
-		self::$icon = get_option( self::ICON );
-		self::$icon2 = get_option( self::ICON2 );
+		self::$password = get_option( self::PASSWORD, 'test123' );
+		self::$passtype = get_option( self::TYPE, 'pass.com.apple.test' );
+		self::$teamid = get_option( self::TEAMID, 'AGK5BZEN3E' );
+		self::$logo = get_option( self::LOGO, GB_PB_PATH . '/temp/logo.png' );
+		self::$icon = get_option( self::ICON, GB_PB_PATH . '/temp/icon.png'  );
+		self::$icon2 = get_option( self::ICON2, GB_PB_PATH . '/temp/icon@2x.png'  );
+		self::$bg = get_option( self::BG, GB_PB_PATH . '/temp/background.png'  );
+
 		// Options
 		add_action( 'admin_init', array( get_class(), 'register_settings_fields' ), 10, 0 );
 	}
 
 	public static function register_settings_fields() {
 		$page = Group_Buying_UI::get_settings_page();
-		$section = 'gb_passbook_voucher_settings';
+		$section = 'gb_passbook_vouchers_settings';
 		add_settings_section( $section, self::__( 'Passbook Vouchers' ), array( get_class(), 'display_settings_section' ), $page );
 		// Settings
 		register_setting( $page, self::PASSWORD );
@@ -39,14 +43,16 @@ class GBS_Passbook_Options extends GBS_Passbook_Addon {
 		register_setting( $page, self::LOGO );
 		register_setting( $page, self::ICON );
 		register_setting( $page, self::ICON2 );
+		register_setting( $page, self::BG );
 
 		// Fields
 		add_settings_field( self::PASSWORD, self::__( 'Certificate Password' ), array( get_class(), 'display_option' ), $page, $section );
 		add_settings_field( self::TYPE, self::__( 'passTypeIdentifier' ), array( get_class(), 'display_type' ), $page, $section );
 		add_settings_field( self::TEAMID, self::__( 'teamIdentifier' ), array( get_class(), 'display_teamid' ), $page, $section );
-		add_settings_field( self::LOGO, self::__( 'Passbook Logo Url' ), array( get_class(), 'display_logo' ), $page, $section );
+		add_settings_field( self::LOGO, self::__( 'Passbook Logo' ), array( get_class(), 'display_logo' ), $page, $section );
 		add_settings_field( self::ICON, self::__( 'Passbook Icon' ), array( get_class(), 'display_icon' ), $page, $section );
 		add_settings_field( self::ICON2, self::__( 'Passbook Icon@2x (retina)' ), array( get_class(), 'display_icon2' ), $page, $section );
+		add_settings_field( self::BG, self::__( 'Passbook Background Image' ), array( get_class(), 'display_bg' ), $page, $section );
 	}
 
 	public function display_settings_section() {
@@ -67,14 +73,22 @@ class GBS_Passbook_Options extends GBS_Passbook_Addon {
 
 	public static function display_logo() {
 		echo '<input name="'.self::LOGO.'" id="'.self::LOGO.'" type="text" value="'.self::$logo.'">';
+		echo '<br/><p class="desc">Full filepath necessary, not a URL.</p>';
 	}
 
 	public static function display_icon() {
 		echo '<input name="'.self::ICON.'" id="'.self::ICON.'" type="text" value="'.self::$icon.'">';
+		echo '<br/><p class="desc">Full filepath necessary, not a URL.</p>';
 	}
 
 	public static function display_icon2() {
 		echo '<input name="'.self::ICON2.'" id="'.self::ICON2.'" type="text" value="'.self::$icon2.'">';
+		echo '<br/><p class="desc">Full filepath necessary, not a URL.</p>';
+	}
+
+	public static function display_bg() {
+		echo '<input name="'.self::BG.'" id="'.self::BG.'" type="text" value="'.self::$bg.'">';
+		echo '<br/><p class="desc">Full filepath necessary, not a URL.</p>';
 	}
 
 }
--- group-buying-passbook.php
+++ group-buying-passbook.php
@@ -11,9 +11,10 @@ Plugin Author URI: http://sproutventure.com/
 Text Domain: group-buying
 */
 
+define( 'GB_PB_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) );
+define( 'GB_PBLIB_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) . '/lib/' );
+define( 'GB_PBCERT_PATH', WP_CONTENT_DIR . '/gb-passbook/' );
 
-define( 'GB_PBLIB_PATH', WP_PLUGIN_DIR . '/' . basename( dirname( __FILE__ ) ) . 'lib/' );
-define( 'GB_PBCERT_PATH', WP_PLUGIN_DIR . '/gbs-passbook/' );
 
 // Load after all other plugins since we need to be compatible with groupbuyingsite
 add_action( 'plugins_loaded', 'gb_load_passbook' );
